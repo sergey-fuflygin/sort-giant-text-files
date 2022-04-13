cd147888341650c5692e6828114e4af3951437b8
Sergey Fuflygin <sergey@fuflygin.com>
2022-04-13 19:42:12 +0300

    Initial commit of project structure

---
+++ GiantTextFileSorter/Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiantTextFileSorter.Generator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GiantTextFileSorter/Sorter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiantTextFileSorter.Sorter
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
